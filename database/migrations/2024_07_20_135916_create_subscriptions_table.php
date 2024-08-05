deb751346db7dc6cf14bebc3a4251da6fcd8e696
mina <mina899432@gmail.com>
2024-08-05 13:46:11 +0300

    deploy verify otp

--- database/migrations/2024_07_20_135916_create_subscriptions_table.php
+++ database/migrations/2024_07_20_135916_create_subscriptions_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('subscriptions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('app_id')->nullable();
             $table->string('app_name')->nullable();
             $table->string('token')->nullable();
