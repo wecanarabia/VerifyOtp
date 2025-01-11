2312b82b9badc36c4c71d8e1d0848da306e37dc2
mina <mina899432@gmail.com>
2025-01-11 14:08:32 +0200

    test 11/1/2025 11:53

--- app/Strategies/UnformalWhatsappStrategy.php
+++ app/Strategies/UnformalWhatsappStrategy.php
@@ -30,7 +30,7 @@ class UnformalWhatsappStrategy implements SmsInterface
                     'headers' => $this->headers,
                     'json' => "",
                 ]);
-                dd(json_decode($response->getBody()->getContents(), true)["staus"]);
+                dd(json_decode($response->getBody()->getContents(), true)["status"]);
                 return $response->getBody()->getContents();
             }
         } catch (\Exception $e) {
